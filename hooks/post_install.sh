dc657428b28f699fddafd59ed6342c90c7505c51
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-03-22 16:41:14 +0100

    Update post_install.sh
    Update example that is also used on the demo server

--- hooks/post_install.sh
+++ hooks/post_install.sh
@@ -5,6 +5,6 @@ $HESTIA/bin/v-update-letsencrypt-ssl
 # Enable Demo mode
 $HESTIA/bin/v-change-sys-demo-mode yes
 # Update templates to enable default username 
-sed -i "s|autofocus|autofocus value=\"admin\"|g" $HESTIA/web/templates/pages/login/login.html
+sed -i "s|autofocus|autofocus value=\"admin\"|g" $HESTIA/web/templates/pages/login/login.php
 # Update templates to enable default password 
-sed -i "s|autofocus|autofocus value=\"StrongSecurePassword\"|g" $HESTIA/web/templates/pages/login/login_1.html
+sed -i "s|autofocus|autofocus value=\"StrongSecurePassword\"|g" $HESTIA/web/templates/pages/login/login_1.php
